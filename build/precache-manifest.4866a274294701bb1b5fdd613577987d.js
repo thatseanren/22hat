eea2790e2dd390c1a4c3cc0bd051737fdb6c18d3
seanren96 <seanren96@gmail.com>
2019-11-12 15:21:49 +1100

    4rd commit

---
+++ build/precache-manifest.4866a274294701bb1b5fdd613577987d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ead50a4dc99ac4dd08dde5e2ea4b99",
+    "url": "/2hat/index.html"
+  },
+  {
+    "revision": "bd9df42b445fc15611cc",
+    "url": "/2hat/static/css/main.03dc0315.chunk.css"
+  },
+  {
+    "revision": "a39266de48e1c7563f23",
+    "url": "/2hat/static/js/2.e1fee474.chunk.js"
+  },
+  {
+    "revision": "bd9df42b445fc15611cc",
+    "url": "/2hat/static/js/main.16ed1b7e.chunk.js"
+  },
+  {
+    "revision": "3940880fca4a7402d6fb",
+    "url": "/2hat/static/js/runtime-main.d84d5517.js"
+  }
+]);
